25589ef643d4be6be8f917935adca5dfd15e2506
Nipun Garg <nipung271@gmail.com>
2019-10-12 15:56:51 +0530

    Setup CLI framework for Lox interpreter

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::env;
+use std::fs;
+use std::io::ErrorKind;
+
+fn run_prompt() -> Result<(), String> {
+    Ok(())
+}
+
+fn run_file(file: &String) -> Result<(), String> {
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let exit_output = match args.len() {
+        1 => run_prompt(),
+        2 => match fs::read_to_string(&args[1]) {
+            Ok(_) => run_file(&args[1]),
+            Err(err) => match err.kind() {
+                ErrorKind::NotFound => Err(String::from("File not found.")),
+                ErrorKind::PermissionDenied => Err(String::from("Permission denied.")),
+                _ => Err(format!("There was a problem processing the file: {}", err)),
+            },
+        },
+        _ => {
+            println!("Usage: lox [script]");
+            Ok(())
+        }
+    };
+
+    if let Err(e) = exit_output {
+        eprintln!("[Error] Exit Output: {}", e);
+    }
+}
