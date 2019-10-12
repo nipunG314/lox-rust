09fd6226feaefb58fd99fe0ff81d99332a69a66f
Nipun Garg <nipung271@gmail.com>
2019-10-12 16:19:17 +0530

    Add TokenType enum and Token struct

--- src/core.rs
+++ src/core.rs
@@ -7,3 +7,62 @@ pub fn run_file(file: &String) -> Result<(), String> {
     println!("Scan and print Tokens");
     Ok(())
 }
+
+#[derive(Debug)]
+enum TokenType {
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    SemiColon,
+    Slash,
+    Star,
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    Identifier,
+    Str(String),
+    Number(f64),
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+}
+
+#[derive(Debug)]
+struct Token {
+    token_type: TokenType,
+    lexeme: &'static str,
+    line: usize,
+}
+
+impl Token {
+    fn new(token_type: TokenType, lexeme: &'static str, line: usize) -> Token {
+        Token {
+            token_type,
+            lexeme,
+            line,
+        }
+    }
+}
