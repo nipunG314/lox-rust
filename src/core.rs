c6fc8647afda5ece6aea575057dfe8d0da5a782f
Nipun Garg <nipung271@gmail.com>
2019-10-15 01:30:53 +0530

    Add Expr structs for Binary, Unary, Literal and..
    ..Grouping expression types

--- src/core.rs
+++ src/core.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug)]
 pub enum TokenType {
     LeftParen,
@@ -57,4 +59,114 @@ impl Token {
     }
 }
 
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.lexeme)
+    }
+}
+
 pub struct NextTokenInfo(pub char, pub TokenType, pub TokenType);
+
+trait Expr: fmt::Display {}
+
+struct Binary<T>
+where
+    T: Expr,
+{
+    left: T,
+    op: Token,
+    right: T,
+}
+
+impl<T> Binary<T>
+where
+    T: Expr,
+{
+    fn new(left: T, op: Token, right: T) -> Binary<T> {
+        Binary { left, op, right }
+    }
+}
+
+impl<T> fmt::Display for Binary<T>
+where
+    T: Expr,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} {}", self.left, self.op, self.right)
+    }
+}
+
+impl<T> Expr for Binary<T> where T: Expr {}
+
+struct Grouping<T>
+where
+    T: Expr,
+{
+    expression: T,
+}
+
+impl<T> Grouping<T>
+where
+    T: Expr,
+{
+    fn new(expression: T) -> Grouping<T> {
+        Grouping { expression }
+    }
+}
+
+impl<T> fmt::Display for Grouping<T>
+where
+    T: Expr,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({})", self.expression)
+    }
+}
+
+impl<T> Expr for Grouping<T> where T: Expr {}
+
+struct Literal {
+    value: Token,
+}
+
+impl Literal {
+    fn new(value: Token) -> Literal {
+        Literal { value }
+    }
+}
+
+impl fmt::Display for Literal {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value.lexeme)
+    }
+}
+
+impl Expr for Literal {}
+
+struct Unary<T>
+where
+    T: Expr,
+{
+    op: Token,
+    right: T,
+}
+
+impl<T> Unary<T>
+where
+    T: Expr,
+{
+    fn new(op: Token, right: T) -> Unary<T> {
+        Unary { op, right }
+    }
+}
+
+impl<T> fmt::Display for Unary<T>
+where
+    T: Expr,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}", self.op, self.right)
+    }
+}
+
+impl<T> Expr for Unary<T> where T: Expr {}
