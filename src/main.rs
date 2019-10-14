4b5079bc7c4582e545d571b302e572f3c4365617
Nipun Garg <nipung271@gmail.com>
2019-10-14 12:06:21 +0530

    Move Scanner into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
+mod core;
+pub mod error;
+pub mod scanner;
+
+use crate::scanner::Scanner;
 use std::env;
 use std::fs;
 use std::io::ErrorKind;
-
-mod core;
-mod error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
-        1 => core::run_prompt(),
+        1 => run_prompt(),
         2 => match fs::read_to_string(&args[1]) {
-            Ok(source) => core::run_file(source),
+            Ok(source) => run_file(source),
             Err(err) => match err.kind() {
                 ErrorKind::NotFound => println!("{}", String::from("File not found.")),
                 ErrorKind::PermissionDenied => println!("{}", String::from("Permission denied.")),
@@ -22,4 +24,14 @@ fn main() {
             println!("Usage: lox [script]");
         }
     };
+}
+
+fn run_prompt() {
+    println!("Implement REPL");
+}
+
+fn run_file(source: String) {
+    let mut scanner = Scanner::new(&source);
+    scanner.scan_tokens();
+    println!("{:#?}", scanner.get_tokens());
 }
---
+++ src/scanner.rs
@@ -0,0 +1,237 @@
+use crate::core::{NextTokenInfo, Token, TokenType};
+use crate::error;
+use std::iter::Peekable;
+use std::str::Chars;
+
+pub struct Scanner<'a> {
+    source: &'a String,
+    reader: Peekable<Chars<'a>>,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl<'a> Scanner<'a> {
+    pub fn new(source: &'a String) -> Scanner<'a> {
+        let reader = source.chars().peekable();
+
+        Scanner {
+            source,
+            reader,
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1,
+        }
+    }
+
+    pub fn scan_tokens(&mut self) {
+        while let Some(_) = self.reader.peek() {
+            self.start = self.current;
+            self.scan_token();
+        }
+    }
+
+    pub fn get_tokens(&self) -> &Vec<Token> {
+        &self.tokens
+    }
+
+    fn scan_token(&mut self) {
+        use TokenType::*;
+
+        match self.advance().unwrap() {
+            '(' => self.add_token(LeftParen),
+            ')' => self.add_token(RightParen),
+            '{' => self.add_token(LeftBrace),
+            '}' => self.add_token(RightBrace),
+            ',' => self.add_token(Comma),
+            '.' => self.add_token(Dot),
+            '-' => self.add_token(Minus),
+            '+' => self.add_token(Plus),
+            ';' => self.add_token(SemiColon),
+            '*' => self.add_token(Star),
+            '!' => self.add_next_token(NextTokenInfo('=', BangEqual, Bang)),
+            '=' => self.add_next_token(NextTokenInfo('=', EqualEqual, Equal)),
+            '<' => self.add_next_token(NextTokenInfo('=', LessEqual, Less)),
+            '>' => self.add_next_token(NextTokenInfo('=', GreaterEqual, Greater)),
+            '/' => match self.check_next_symbol(|c| c == '/') {
+                None => error::error(self.line, "Unexpected EOF"),
+                Some(false) => self.add_token(Slash),
+                Some(true) => {
+                    while let Some(false) = self.check_next_symbol(|c| c == '\n') {
+                        self.advance();
+                    }
+                    self.line += 1;
+                }
+            },
+            ' ' | '\r' | '\t' => (),
+            '\n' => {
+                self.line += 1;
+            }
+            '"' => {
+                let new_string = self.make_string();
+                if let Some(s) = new_string {
+                    self.add_token(s);
+                }
+            }
+            c => {
+                if c.is_digit(10) {
+                    let new_number = self.make_number();
+                    if let Some(num) = new_number {
+                        self.add_token(num);
+                    }
+                } else if c.is_alphabetic() || c == '_' {
+                    let new_id = self.make_identifier();
+                    self.add_token(new_id);
+                } else {
+                    error::error(self.line, "Unexpected character.");
+                }
+            }
+        }
+    }
+
+    fn advance(&mut self) -> Option<char> {
+        self.current += 1;
+        self.reader.next()
+    }
+
+    // Checks the next symbol and if it satisfies a closure, consumes it
+    fn check_next_symbol<T>(&mut self, f: T) -> Option<bool>
+    where
+        T: Fn(char) -> bool,
+    {
+        if let Some(c) = self.reader.peek() {
+            if f(*c) {
+                self.advance();
+                return Some(true);
+            } else {
+                return Some(false);
+            }
+        }
+        None
+    }
+
+    fn add_token(&mut self, token_type: TokenType) {
+        if let TokenType::Str(_) = token_type {
+            let text = self
+                .source
+                .chars()
+                .skip(self.start + 1)
+                .take((self.current - 1) - (self.start + 1))
+                .collect::<String>();
+            self.tokens.push(Token::new(token_type, text, self.line));
+        } else {
+            let text = self
+                .source
+                .chars()
+                .skip(self.start)
+                .take(self.current - self.start)
+                .collect::<String>();
+            self.tokens.push(Token::new(token_type, text, self.line));
+        }
+    }
+
+    fn add_next_token(&mut self, next_token: NextTokenInfo) {
+        let NextTokenInfo(expected, token_type1, token_type2) = next_token;
+        if let Some(true) = self.check_next_symbol(|c| c == expected) {
+            self.add_token(token_type1);
+        } else {
+            self.add_token(token_type2);
+        }
+    }
+
+    fn make_string(&mut self) -> Option<TokenType> {
+        loop {
+            match self.check_next_symbol(|c| c == '"') {
+                None => {
+                    error::error(self.line, "Unterminated string.");
+                    return None;
+                }
+                Some(false) => {
+                    if let Some(true) = self.check_next_symbol(|c| c == '\n') {
+                        self.line += 1;
+                    }
+                    self.advance();
+                }
+                Some(true) => {
+                    break;
+                }
+            }
+        }
+
+        let literal_value = self
+            .source
+            .chars()
+            .skip(self.start + 1)
+            .take((self.current - 1) - (self.start + 1))
+            .collect::<String>();
+
+        Some(TokenType::Str(literal_value))
+    }
+
+    fn make_number(&mut self) -> Option<TokenType> {
+        loop {
+            match self.check_next_symbol(|c| c.is_digit(10)) {
+                Some(false) => {
+                    if let Some(true) = self.check_next_symbol(|c| c == '.') {
+                        if let Some(false) | None = self.check_next_symbol(|c| c.is_digit(10)) {
+                            error::error(self.line, "Number cannot end with '.' operator");
+                            return None;
+                        }
+                        while let Some(true) = self.check_next_symbol(|c| c.is_digit(10)) {}
+                    }
+                    break;
+                }
+                Some(true) => {
+                    continue;
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+
+        let literal_value = self
+            .source
+            .chars()
+            .skip(self.start)
+            .take((self.current) - (self.start))
+            .collect::<String>();
+
+        let literal_value: f64 = literal_value.parse().unwrap();
+
+        Some(TokenType::Number(literal_value))
+    }
+
+    fn make_identifier(&mut self) -> TokenType {
+        while let Some(true) = self.check_next_symbol(|c| c.is_alphanumeric() || c == '_') {}
+
+        let literal_value = self
+            .source
+            .chars()
+            .skip(self.start)
+            .take((self.current) - (self.start))
+            .collect::<String>();
+
+        match literal_value.as_ref() {
+            "and" => TokenType::And,
+            "class" => TokenType::Class,
+            "else" => TokenType::Else,
+            "false" => TokenType::False,
+            "for" => TokenType::For,
+            "fun" => TokenType::Fun,
+            "if" => TokenType::If,
+            "nil" => TokenType::Nil,
+            "or" => TokenType::Or,
+            "print" => TokenType::Print,
+            "return" => TokenType::Return,
+            "super" => TokenType::Super,
+            "this" => TokenType::This,
+            "true" => TokenType::True,
+            "var" => TokenType::Var,
+            "while" => TokenType::While,
+            _ => TokenType::Identifier,
+        }
+    }
+}
