8ef65944c87df4c8b695c38259ea2e4486997381
Nipun Garg <nipung271@gmail.com>
2019-12-30 12:44:57 +0530

    Add token_error() to report ParseErrors

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,18 @@
+use crate::core::{Token, TokenType};
+
 fn report(line: usize, location: &str, message: &str) {
     eprintln!("[line {}] Error{}: {}", line, location, message);
 }
 
 pub fn error(line: usize, message: &str) {
     report(line, "", message);
+}
+
+pub fn token_error(token: &Token, message: &str) {
+    if token.token_type == TokenType::EOF {
+        report(token.line, " at end", message);
+    } else {
+        let location = " at '".to_string() + &token.lexeme + "'";
+        report(token.line, &location, message);
+    }
 }
