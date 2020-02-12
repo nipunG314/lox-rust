afabacedd629c7cd92e902da7d0e7e3a7e98fa4f
Nipun Garg <nipung271@gmail.com>
2020-02-12 19:02:00 +0530

    Simplify IOError reporting

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ fn main() {
         2 => match fs::read_to_string(&args[1]) {
             Ok(source) => run_file(source),
             Err(err) => match err.kind() {
-                ErrorKind::NotFound => println!("{}", String::from("File not found.")),
-                ErrorKind::PermissionDenied => println!("{}", String::from("Permission denied.")),
+                ErrorKind::NotFound => println!("Couldn't find: {}", args[1]),
+                ErrorKind::PermissionDenied => println!("Permission denied."),
                 _ => println!("There was a problem processing the file: {}", err),
             },
         },
